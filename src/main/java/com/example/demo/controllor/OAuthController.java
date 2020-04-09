c7a8e1b0b17f2e09f7766b020bec50f29c43fb13
张烁 <zhangshuo@zhangshuodeMBP.lan>
2020-04-09 18:06:26 +0800

    lonin

--- src/main/java/com/example/demo/controllor/OAuthController.java
+++ src/main/java/com/example/demo/controllor/OAuthController.java
@@ -4,11 +4,13 @@ import com.example.demo.dto.AccessTokenDTO;
 import com.example.demo.dto.GithubUser;
 import com.example.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Controller
@@ -16,21 +18,31 @@ public class OAuthController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state, Model model) throws IOException {
+                           @RequestParam(name="state") String state, Model model,
+                           HttpServletRequest request) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("8f7662daa403740303e6");
-        accessTokenDTO.setClient_secret("9067b5e3629c10450ce4d148f3caee9ee2211828");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         try{
             String accessToken = githubProvider.getAccessToken(accessTokenDTO);
             GithubUser githubUser = githubProvider.getUser(accessToken);
-            model.addAttribute("username", githubUser.getLogin());
+            if(githubUser != null){
+                request.getSession().setAttribute("githubUser", githubUser);
+            }
+            //model.addAttribute("username", githubUser.getLogin());
         }
         catch (IOException e){
             return "error";
         }
-        return "index";
+        return "redirect:/";
+//        return "index";
     }
 }
--- src/main/java/com/example/demo/dto/AccessTokenDTO.java
+++ src/main/java/com/example/demo/dto/AccessTokenDTO.java
@@ -7,4 +7,8 @@ public class AccessTokenDTO {
     private String client_id;
     private String client_secret;
     private String code;
+    private String redirect_uri;
+    private String state;
+
+
 }
