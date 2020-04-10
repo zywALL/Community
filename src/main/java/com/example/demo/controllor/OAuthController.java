11a97f87edbc3b28e6d987f84dc0a7f286ce2086
张烁 <zhangshuo@zhangshuodeMBP.lan>
2020-04-11 01:56:29 +0800

    cookie&session

--- src/main/java/com/example/demo/controllor/OAuthController.java
+++ src/main/java/com/example/demo/controllor/OAuthController.java
@@ -12,7 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -32,7 +34,7 @@ public class OAuthController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state, Model model,
-                           HttpServletRequest request) throws IOException {
+                           HttpServletResponse response) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -44,13 +46,13 @@ public class OAuthController {
                 User user = new User();
                 user.setAccount_id(String.valueOf(githubUser.getId()));
                 user.setName(githubUser.getLogin());
-                user.setToken(UUID.randomUUID().toString());
+                String token = UUID.randomUUID().toString();
+                user.setToken(token);
                 user.setGmt_create(System.currentTimeMillis());
                 user.setGmt_modified(user.getGmt_create());
                 userMapper.insert(user);
-                request.getSession().setAttribute("githubUser", githubUser);
+                response.addCookie(new Cookie("token", token));
             }
-            //model.addAttribute("username", githubUser.getLogin());
         }
         catch (IOException e){
             return "error";
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -3,9 +3,17 @@ package com.example.demo.mapper;
 import com.example.demo.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface UserMapper {
     @Insert("INSERT INTO USER (account_id, name, token, gmt_create, gmt_modified)\n" + "VALUES\n" + "(#{account_id}, #{name}, #{token}, #{gmt_create}, #{gmt_modified});")
     void insert(User user);
+
+    @Select("SELECT * FROM USER WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
 }
