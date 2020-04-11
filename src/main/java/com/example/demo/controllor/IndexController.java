aca23acecb6c25f75088acde77d106179b3d8114
张烁 <zhangshuo@zhangshuodeMBP.lan>
2020-04-11 17:46:04 +0800

    a

--- src/main/java/com/example/demo/controllor/IndexController.java
+++ src/main/java/com/example/demo/controllor/IndexController.java
@@ -18,20 +18,22 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request){
-
-        Cookie[] cookies = request.getCookies();
-        if(cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if(user != null){
-                        request.getSession().setAttribute("user", user);
+        if(request.getCookies() != null){
+            Cookie[] cookies = request.getCookies();
+            if(cookies.length != 0) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("token")) {
+                        String token = cookie.getValue();
+                        User user = userMapper.findByToken(token);
+                        if(user != null){
+                            request.getSession().setAttribute("user", user);
+                        }
+                        break;
                     }
-                    break;
                 }
             }
         }
+
         return "index";
     }
 }
